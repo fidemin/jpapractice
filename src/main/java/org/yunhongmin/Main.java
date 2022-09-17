c719a2a9062cd5f50756dcf303b54b0e496f04c3
Yunhong Min <yunhongmin00@gmail.com>
2022-09-17 20:57:54 +0900

    import organization

--- src/main/java/org/yunhongmin/Main.java
+++ src/main/java/org/yunhongmin/Main.java
@@ -1,13 +1,5 @@
 package org.yunhongmin;
 
-import org.yunhongmin.entity.Member;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
     }
