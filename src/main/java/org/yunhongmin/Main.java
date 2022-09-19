011083d6c64a40b5c02a9bbfaa8a65738dd39450
Yunhong Min <yunhongmin00@gmail.com>
2022-09-19 20:13:12 +0900

    shop entity relations

--- src/main/java/org/yunhongmin/Main.java
+++ src/main/java/org/yunhongmin/Main.java
@@ -5,8 +5,7 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpapractice");
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
         emf.close();
     }
 }
