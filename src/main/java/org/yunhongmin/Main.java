ab79271ccbc528aa7f8ba7e890db408e970fefdd
Yunhong Min <yunhongmin00@gmail.com>
2022-09-29 01:53:59 +0900

    spring + gradle setting

--- src/main/java/org/yunhongmin/Main.java
+++ src/main/java/org/yunhongmin/Main.java
@@ -5,7 +5,5 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
-        emf.close();
     }
 }
