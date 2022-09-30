cd9b7f96736d317e4f6c8d30ddfaf4b74a6b3c0b
Yunhong Min <yunhongmin00@gmail.com>
2022-09-30 19:26:03 +0900

    join duplicate user case

--- src/test/java/org/yunhongmin/shop/service/UserServiceTest.java
+++ src/test/java/org/yunhongmin/shop/service/UserServiceTest.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.yunhongmin.shop.domain.User;
+import org.yunhongmin.shop.repository.UserRepository;
 
 import javax.transaction.Transactional;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:appConfig.xml")
 @Transactional
 public class UserServiceTest {
     @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
 
     @Test
     public void joinSuccess() {
@@ -24,8 +28,29 @@ public class UserServiceTest {
         user.setName(name);
 
         // When
-        Long userId = userService.join(user);
+        userService.join(user);
+
+        // Then
         assertEquals(name, user.getName());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void joinFailure() {
+        // Given
+        User user = new User();
+        String name = "yunhong";
+        user.setName(name);
+        userRepository.save(user);
+
+        // When
+        User dupUser = new User();
+        user.setName(name);
+        userService.join(user);
+
+        // Then
+        fail("IllegalArgumentException required");
+    }
+
+
 
 }
