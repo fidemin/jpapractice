90281869504f2ee6073a4d91888593a93e6118c7
Yunhong Min <yunhongmin00@gmail.com>
2022-10-07 20:23:27 +0900

    applies JpaRepository:UserRepository

--- src/main/java/org/yunhongmin/shop/repository/UserRepository.java
+++ src/main/java/org/yunhongmin/shop/repository/UserRepository.java
@@ -1,34 +1,10 @@
 package org.yunhongmin.shop.repository;
 
-import org.springframework.stereotype.Repository;
-import org.yunhongmin.practice.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.yunhongmin.shop.domain.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-public class UserRepository {
-    @PersistenceContext
-    EntityManager em;
-
-    public void save(User user) {
-        em.persist(user);
-    }
-
-    public User findOne(Long id) {
-        return em.find(User.class, id);
-    }
-
-    public List<User> findAll() {
-        return em.createQuery("select u from User u", User.class)
-                .getResultList();
-    }
-
-    public List<User> findByName(String name) {
-        return em.createQuery("select u from User u where u.name = :name", User.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
 }
