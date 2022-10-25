ecd1419dd57c00c5ddc03979d0747a389be5ae69
Yunhong Min <yunhongmin00@gmail.com>
2022-10-26 00:47:52 +0900

    apply logger

--- src/main/java/org/yunhongmin/shop/aspect/PerformanceAspect.java
+++ src/main/java/org/yunhongmin/shop/aspect/PerformanceAspect.java
@@ -3,16 +3,21 @@ package org.yunhongmin.shop.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 
 @Aspect
 @Component
 public class PerformanceAspect {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Around("@annotation(org.yunhongmin.shop.annotation.PerformanceLogging)")
     public Object logPerformance(ProceedingJoinPoint pjp) throws Throwable {
         long begin = System.currentTimeMillis();
         Object result = pjp.proceed();
-        System.out.println("execute time: " + (System.currentTimeMillis() - begin));
+        logger.info("execute time: " + (System.currentTimeMillis() - begin));
         return result;
     }
 }
